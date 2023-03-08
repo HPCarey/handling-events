7fa47fca1fce5c00458469c9c1a7999f9cd6b743
HPCarey <hannahpcarey@gmail.com>
2023-03-08 17:44:36 +0000

    methods as props

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import EventsFunctional from "./components/EventsFunctional";
 import EventBinding from "./components/EventBinding";
 import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
 import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
-
+import NestingComponents from "./components/NestingComponents";
+import MethodsAsPropsParent from "./components/MethodsAsPropsParent";
 
 function App() {
   return <div className="App">
     {/* <EventsFunctional />
     <EventsClass /> */}
     {/* <EventBinding /> */}
-    <ConditionalRenderingClass />
-    <ConditionalRenderingFunctional connected={true}/>
+    {/* <ConditionalRenderingClass />
+    <ConditionalRenderingFunctional connected={true}/> */}
+    {/* <NestingComponents /> */}
+    <MethodsAsPropsParent />
   </div>;
 }
 
---
+++ src/components/ES7Snippets.js
@@ -0,0 +1,67 @@
+// rfce
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+
+
+
+// rafce => arrow function
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+
+
+
+// creates and exports a class component
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+
+
+
+// rconst : snippet for creating constructor
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+
+
+
+//clg logs something to the console
+// console.log(first)
+
+
+
+
+// clo logs an object along with it's name as a string
+// console.log('first', first)
+
