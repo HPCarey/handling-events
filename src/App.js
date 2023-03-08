1cc8413e79176aaa2fbf19f1ddcc7065efe92587
HPCarey <hannahpcarey@gmail.com>
2023-03-08 15:15:13 +0000

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
 import EventsClass from "./components/EventsClass";
 import EventsFunctional from "./components/EventsFunctional";
+import EventBinding from "./components/EventBinding";
+import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
+import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
+
 
 function App() {
   return <div className="App">
-    <EventsFunctional />
-    <EventsClass />
+    {/* <EventsFunctional />
+    <EventsClass /> */}
+    {/* <EventBinding /> */}
+    <ConditionalRenderingClass />
+    <ConditionalRenderingFunctional connected={true}/>
   </div>;
 }
 
