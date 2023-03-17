019231176dc3708a49583a6a6d2c994269bca859
HPCarey <hannahpcarey@gmail.com>
2023-03-17 15:06:33 +0000

    running effects only once

--- src/components/UseEffectCounter.js
+++ src/components/UseEffectCounter.js
@@ -2,9 +2,22 @@ import React, {useState, useEffect} from 'react'
 
 function UseEffectCounter() {
     const [count, setCount] = useState(0);
+    const [time, setTime] = useState(0);
     useEffect(() => {
-        document.title = count
-    });
+        console.log('count 1 effect');
+        document.title = count;
+    }, [count]);
+    useEffect(() => {
+        console.log('creating timer');
+        const interval = setInterval(() => {
+            setTime(time => time + 1)
+        }, 1000);
+    }, []);
+    // const [count2, setCount2] = useState(0);
+    // useEffect(() => {
+    //     console.log('count 2 effect');
+    //     document.title = count2;
+    // }, [count2]);
   return (
     <div>
         <button onClick={
@@ -12,6 +25,12 @@ function UseEffectCounter() {
         }>
             Inrement Count ({count})
         </button>
+        <h2>Time is {time}</h2>
+        {/* <button onClick={
+            () => setCount2(count2 => count2 + 1)
+        }>
+            Inrement Count2 ({count2})
+        </button> */}
     </div>
   )
 }
