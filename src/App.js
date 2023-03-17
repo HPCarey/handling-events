114eb7f1b02eaac7355ebe26d84450c310738ce9
HPCarey <hannahpcarey@gmail.com>
2023-03-17 14:22:37 +0000

    using hooks to update state of an array

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ControlledForm from "./components/ControlledForm";
 import UncontrolledForm from "./components/UncontrolledForm";
 import SearchBar from "./components/SearchBar";
 import ControlledFormHooks from "./components/ControlledFormHooks";
+import UseStateWithArrays from "./components/UseStateWithArrays";
 
 function App() {
   return <div className="App">
@@ -31,7 +32,8 @@ function App() {
     {/* <ControlledForm /> */}
     {/* <UncontrolledForm /> */}
     {/* <SearchBar /> */}
-    <ControlledFormHooks />
+    {/* <ControlledFormHooks /> */}
+    <UseStateWithArrays />
   </div>;
 }
 
