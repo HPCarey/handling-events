76b3b7d6d88788682b8cb2fef57ab29035d32583
HPCarey <hannahpcarey@gmail.com>
2023-03-17 15:34:41 +0000

    effect cleanup functions

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import ControlledFormHooks from "./components/ControlledFormHooks";
 import UseStateWithArrays from "./components/UseStateWithArrays";
 import UseStateWithObjects from "./components/UseStateWithObjects";
 import UseEffectCounter from "./components/UseEffectCounter";
+import UseEffectCounterContainer from "./components/UseEffectCounterContainer";
 
 function App() {
   return <div className="App">
@@ -37,7 +38,8 @@ function App() {
     {/* <ControlledFormHooks /> */}
     {/* <UseStateWithArrays /> */}
     {/* <UseStateWithObjects /> */}
-    <UseEffectCounter />
+    {/* <UseEffectCounter /> */}
+    <UseEffectCounterContainer />
   </div>;
 }
 
